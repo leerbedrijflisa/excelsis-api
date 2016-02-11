66f0be1cc89153db5ac581ad81f11a6621bde597
Ramon <ramon.meijers@hotmail.com>
2016-02-11 14:45:17 +0100

    Startup of the assessmentscontroller and the get method

--- src/Excelsis.Api/controllers/AssessmentsController.cs
+++ src/Excelsis.Api/controllers/AssessmentsController.cs
@@ -3,31 +3,33 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Excelsis.Api;
 
 namespace Excelsis.Api
 {
     [Route("/assessments/")]
-    public class Assessments
+    public class AssessmentsController
     {
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
-            //var assessments = await FetchAssessments();
-
-            return new HttpOkObjectResult("ok");
+            var assessments = await FetchAssessments();
+            //var result = ConvertAssessments(assessments);
+            
+            return new HttpOkObjectResult(assessments);
         }
 
-        //private async Task<IEnumerable<object>> FetchAssessments()
-        //{
-        //    var account = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
-        //    var client = account.CreateCloudTableClient();
-        //    var table = client.GetTableReference("assessments");
-        //    await table.CreateIfNotExistsAsync();
+        private async Task<IEnumerable<object>> FetchAssessments()
+        {
+            var account = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+            var client = account.CreateCloudTableClient();
+            var table = client.GetTableReference("assessments");
+            await table.CreateIfNotExistsAsync();
 
-        //    var query = new TableQuery<DynamicEntity>();
-        //    var assessments = table.ExecuteQuerySegmentedAsync(query, null);
+            var query = new TableQuery<DynamicEntity>();
+            var assessments = table.ExecuteQuerySegmentedAsync(query, null);
 
-        //    return assessments.Result;
-        //}
+            return assessments.Result;
+        }
     }
 }
---
+++ src/Excelsis.Api/mappers/AssessmentsMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Excelsis.Api
+{
+    public class AssessmentsMapper
+    {
+        public IEnumerable<DynamicEntity> ConvertAssessments(dynamic assessments)
+        {
+            return null;
+        }
+    }
+}
