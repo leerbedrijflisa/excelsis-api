2ffa18689123f02b4d1ee4544074f41b582bd3e9
Ramon <ramon.meijers@hotmail.com>
2016-04-08 13:14:58 +0200

    Did some things to make a funtionality

--- Lisa.Excelsis.Api/FilterHandler.cs
+++ Lisa.Excelsis.Api/FilterHandler.cs
@@ -1,6 +1,5 @@
 ﻿using Lisa.Common.WebApi;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Lisa.Excelsis.Api
 {
@@ -15,8 +14,27 @@ namespace Lisa.Excelsis.Api
                 {
                     if (filter.Type == "StudentName")
                     {
-                        //you are should be to filter on multiple students
-                        if (assessment.StudentName == filter.Value)
+
+                        if (filter.Value.Contains("+") )
+                        {
+                            return null;
+                        }
+
+                        if (filter.Value.Contains(","))
+                        {
+                            var studentNames = filter.Value.Split(',');
+                            foreach (var studentName in studentNames)
+                            {
+                                if (studentName.ToLower() == assessment.StudentName.ToLower())
+                                {
+                                    result.Add(assessment);
+                                    break;
+                                }
+                            }
+                        }
+
+                        //you should be to filter on multiple students
+                        if (assessment.StudentName.ToLower() == filter.Value.ToLower())
                         {
                             result.Add(assessment);
                             break;
@@ -24,13 +42,40 @@ namespace Lisa.Excelsis.Api
                     }
                     if (filter.Type == "Assessors")
                     {
-                        //you should be able to filter on multiple assessors
+                        if (filter.Value.Contains(","))
+                        {
+                            var assessorNames = filter.Value.Split(',');
+                            foreach (var assessorName in assessorNames)
+                            {
+                                if (assessment.Assessors.Contains(assessorName))
+                                {
+                                    result.Add(assessment);
+                                    break;
+                                }
+                            }
+                        }
 
+                        if (filter.Value.Contains(";"))
+                        {
+                            string[] assessorNames = filter.Value.Split(';');
+                            if (assessment.Assessors.Any(assessorNames.Contains))
+                            {
+                                result.Add(assessment);
+                                break;
+                            }
+                        }
+                        
+                        if (assessment.Assessors.ToLower().Contains(filter.Value.ToLower()))
+                        {
+
+                            result.Add(assessment);
+                            break;
+                        }
                     }
                 }
             }
             
-            return null;
+            return result;
         }
     }
 }
