185f8e9c426f5ef630e9a1c29519ec0b32036760
Ramon <ramon.meijers@hotmail.com>
2016-04-13 14:45:29 +0200

    Made the assessors functionality for the filter

--- Lisa.Excelsis.Api/FilterHandler.cs
+++ Lisa.Excelsis.Api/FilterHandler.cs
@@ -10,7 +10,7 @@ namespace Lisa.Excelsis.Api
         public static IEnumerable<DynamicModel> filterAssessments(IEnumerable<DynamicModel> assessments, IEnumerable<Filter> filters)
         {
             var result = new List<DynamicModel>();
-            var filterValues = new List<Filter>();
+            var filterValues = new Dictionary<string, Filter>();
 
             foreach (Filter filter in filters)
             {
@@ -19,11 +19,11 @@ namespace Lisa.Excelsis.Api
                     if (filter.Value.Contains(","))
                     {
                         string studentNames = JsonConvert.SerializeObject(filter.Value.Split(','));
-                        filterValues.Add(new Filter("StudentName", studentNames));
+                        filterValues["StudentName"] = new Filter("ARRAY", studentNames);
                     }
                     else
                     {
-                        filterValues.Add(new Filter("StudentName", filter.Value));
+                        filterValues["StudentName"] = new Filter("SINGLE", filter.Value);
                     }
                 }
 
@@ -32,26 +32,94 @@ namespace Lisa.Excelsis.Api
                     if (filter.Value.Contains(","))
                     {
                         string assessorNames = JsonConvert.SerializeObject(filter.Value.Split(','));
-                        filterValues.Add(new Filter("Assessors,OR", assessorNames));
+                        filterValues["Assessors"] = new Filter("OR", assessorNames);
                     }
                     else if (filter.Value.Contains(";"))
                     {
                         string assessorNames = JsonConvert.SerializeObject(filter.Value.Split(';'));
-                        filterValues.Add(new Filter("Assessors,AND", assessorNames));
+                        filterValues["Assessors"] = new Filter("AND", assessorNames);
                     }
                     else
                     {
-                        filterValues.Add(new Filter("Assessors,SINGLE", filter.Value));
+                        filterValues["Assessors"] = new Filter("SINGLE", filter.Value);
                     }
                 }
             }
 
             foreach (dynamic assessment in assessments)
             {
-                string k = "m"; 
+                int addToResult = 0;
+                if (filterValues.ContainsKey("StudentName"))
+                {
+                    //if studentname == filtervalue studentname add to result
+                }
+
+                if (filterValues.ContainsKey("Assessors") && addToResult != 2)
+                {
+                    addToResult = 0;
+                    if (AssessorsCheck(filterValues["Assessors"], assessment))
+                    {
+                        addToResult = 1;
+                    }
+                }
+
+                if (addToResult == 1)
+                {
+                    result.Add(assessment);
+                }
+
             }
             
             return result;
+        }
+
+        public static bool AssessorsCheck(Filter assessors, dynamic assessment)
+        {
+            var caseInsensitive = JsonConvert.DeserializeObject<List<string>>(JsonConvert.SerializeObject(assessment.Assessors).ToLower());
+            if (assessors.Type == "OR")
+            {
+                var assessorsArray = JsonConvert.DeserializeObject<List<string>>(assessors.Value);
+                foreach (var item in assessorsArray)
+                {
+                    if (caseInsensitive.Contains(item.ToLower()))
+                    {
+                        return true;
+                    }
+                }
+            }
+            else if (assessors.Type == "AND")
+            {
+                var assessorsArray = JsonConvert.DeserializeObject<List<string>>(assessors.Value);
+                bool containsAll = true;
+                foreach (var item in assessorsArray)
+                {
+                    if (!caseInsensitive.Contains(item.ToLower()))
+                    {
+                        containsAll = false;
+                    }
+                }
+                if (containsAll)
+                {
+                    return true;
+                }
+            }
+            else if (assessors.Type == "SINGLE")
+            {
+                if (caseInsensitive.Contains(assessors.Value.ToLower()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool StudentNameCheck(Filter studentName, dynamic assessment)
+        {
+            if (studentName.Type == "ARRAY")
+            {
+                
+            }
+            return false;
         }
     }
 }
