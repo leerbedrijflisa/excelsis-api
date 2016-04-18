2e0dd3f29926e77bf6ce4ab7e9ab0f229a52de3c
Wilco Bakker <99027394@mydavinci.nl>
2016-04-18 10:18:27 +0200

    Feature #14 - Observations can now contain remarks

--- Lisa.Excelsis.Api/AssessmentValidator.cs
+++ Lisa.Excelsis.Api/AssessmentValidator.cs
@@ -19,68 +19,7 @@ namespace Lisa.Excelsis.Api
             Allow("observations");
             Allow("criteria");
         }
-        private void ValidateList(string fieldName, object value)
-        {
-            string allowed;
-            string[] expectedValues = new string[] { "seen", "not seen", "not rated" };
-            allowed = "seen, not seen and not rated";
 
-            if ((value == null) ||
-                (value is JArray) && ((JArray)value).Count == 0)
-            {
-                var error = new Error
-                {
-                    Code = ErrorCode.EmptyValue,
-                    Message = $"The field '{fieldName}' should not be empty",
-                    Values = new
-                    {
-                        Field = fieldName
-                    }
-                };
-                Result.Errors.Add(error);
-                return;
-            }
-            
-            if (value.GetType().Name != "JArray")
-            {
-                var error = new Error
-                {
-                    Code = 10,
-                    Message = $"The field '{fieldName}' doesn't expects the value '{value}' in it's array. Only '{allowed}' allowed.",
-                    Values = new
-                    {
-                        Field = fieldName
-                    }
-                };
-                Result.Errors.Add(error);
-                return;
-            }
-
-            foreach(string val in (JArray)value)
-            {
-                bool expected = false;
-                foreach(string expectedValue in expectedValues)
-                {
-                    if (val == expectedValue)
-                    {
-                        expected = true;
-                    }
-                }
-                if (expected == false)
-                {
-                    var error = new Error
-                    {
-                        Code = 10,
-                        Message = $"The field '{fieldName}' doesn't expects the value '{val}' in it's array. Only {allowed} allowed.",
-                        Values = new
-                        {
-                            Field = fieldName
-                        }
-                    };
-                    Result.Errors.Add(error);
-                }
-            }
-        }
         protected override void ValidateModel()
         {
             Ignore("id");
@@ -92,7 +31,7 @@ namespace Lisa.Excelsis.Api
             Required("examName", NotEmpty);
             Optional("assessors", NotEmpty);
             Optional("assessed", NotEmpty);
-            Optional("observations", ValidateList);
+            Optional("observations", NotEmpty);
             Required("norm", NotEmpty);
             Required("criteria", NotEmpty);
         }
