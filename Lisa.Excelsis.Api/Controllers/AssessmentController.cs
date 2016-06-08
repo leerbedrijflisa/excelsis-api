bed78c0a02257ee10b1a7845a8736b3111d2e7dd
Wilco Bakker <99027394@mydavinci.nl>
2016-06-08 12:43:32 +0200

    #23 - Sort assessments initial commit (completed)

--- Lisa.Excelsis.Api/Controllers/AssessmentController.cs
+++ Lisa.Excelsis.Api/Controllers/AssessmentController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Lisa.Common.WebApi;
 using Microsoft.AspNet.Mvc;
+using System.Linq;
 
 namespace Lisa.Excelsis.Api
 {
@@ -12,10 +13,38 @@ namespace Lisa.Excelsis.Api
         {
             _db = database;
         }
-
+        
         [HttpGet]
-        public async Task<ActionResult> Get()
+        public async Task<ActionResult> Get([FromQuery] string sort, [FromQuery] string order)
         {
+            if (!string.IsNullOrWhiteSpace(sort))
+            {
+                var sorting = sort.Split(new[] { ',' });
+                string[] ordering = new string[sorting.Length];
+                for (int i = 0; i < ordering.Length; i++)
+                {
+                    ordering[i] = "asc";
+                }
+                if (!string.IsNullOrWhiteSpace(order))
+                {
+                    int orderCount = 0;
+                    foreach (var orderValue in order.Split(new[] { ',' }))
+                    {
+                        if (orderCount == sorting.Length)
+                        {
+                            break;
+                        }
+                        ordering[orderCount] = orderValue;
+                        orderCount++;
+                    }
+                }
+
+                Sorting sortingObj = new Sorting(sorting, ordering);
+
+                var result = await _db.FetchAssessments(sortingObj);
+
+                return new HttpOkObjectResult(result);
+            }
             var assessments = await _db.FetchAssessments();
             return new HttpOkObjectResult(assessments);
         }
